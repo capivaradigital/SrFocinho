04fc0320ba619cf1055673a9a4dd16cad85c5080
tmilitino <tmilitino.tm@gmail.com>
2017-07-23 17:51:14 -0300

    codigo base do LOCATION

--- app/src/main/java/com/capivaradigital/srfocinho/Mapa.java
+++ app/src/main/java/com/capivaradigital/srfocinho/Mapa.java
@@ -1,13 +1,57 @@
 package com.capivaradigital.srfocinho;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class Mapa extends AppCompatActivity {
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class Mapa extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
